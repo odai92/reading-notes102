255f5c33e307cc1e953600163f3cac4bd303b71d
Odai Qudah <odai92@live.com>
2020-12-19 23:18:34 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
-let name = prompt("What is your name?");
-alert('Hello,' + name);
+
+
+
+// let name = prompt("What is your name?");
+// alert('Hello,' + name);
+
+var name, password, age;
+let urAge = prompt("how old r u?")
+let enterName = prompt("insert ur name:");
+let enterPassword = prompt("Insert ur password:");
+alert("welcome, " + enterName);
+
+
